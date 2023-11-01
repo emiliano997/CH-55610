6ae2319a2c04feda413a671f5dfbd6d8a2c8101f
Emiliano Perez <emi.perez997@gmail.com>
2023-11-01 10:59:03 -0300

    [ADD] Clase 4

---
+++ clase_4/ejemplos/fsCallback.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+fs.writeFile("./texto_callback.txt", "Escribiendo en un callback", (error) => {
+  if (error) {
+    return console.log("Hubo un error al escribir el archivo");
+  }
+
+  fs.readFile("./texto_callbac.txt", "utf-8", (error, contenido) => {
+    if (error) {
+      return console.log("Hubo un error al leer el archivo");
+    }
+
+    console.log(contenido);
+
+    fs.appendFile(
+      "./texto_callback.txt",
+      "\nTexto recién salido del horno",
+      (error) => {
+        if (error) {
+          return console.log("Hubo un error al agregar contenido al archivo");
+        }
+
+        fs.readFile("./texto_callback.txt", "utf-8", (error, contenido) => {
+          if (error) {
+            return console.log("Hubo un error al leer el archivo");
+          }
+
+          console.log(contenido);
+
+          fs.unlink("./texto_callback.txt", (error) => {
+            if (error) {
+              return console.log("Hubo un error al eliminar el archivo");
+            }
+          });
+        });
+      }
+    );
+  });
+});
