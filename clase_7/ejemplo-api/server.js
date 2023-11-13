463d2c08e58b911c17baf593eefbd0ad71eb84c1
Emiliano Perez <emi.perez997@gmail.com>
2023-11-13 11:21:38 -0300

    [ADD] Clase 7

---
+++ clase_7/ejemplo-api/server.js
@@ -0,0 +1,61 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Datos (en memoria)
+const usuarios = [];
+
+app.get("/", (req, res) => {
+  res.json({
+    mensaje: "Bienvenido a mi api",
+  });
+});
+
+// Endpoints usuarios
+// Obtener usuarios
+app.get("/usuarios", (req, res) => {
+  res.json({
+    usuarios,
+  });
+});
+
+// Obtener usuarios por id
+app.get("/usuarios/:id", (req, res) => {
+  const { id } = req.params;
+
+  const usuario = usuarios.find((user) => user.id === Number(id));
+
+  if (!usuario) {
+    return res.json({
+      error: "User does not exists",
+    });
+  }
+
+  res.json({
+    usuario,
+  });
+});
+
+// Crear usuario
+app.post("/usuarios", (req, res) => {
+  console.log(req.body);
+
+  const { id, username, name } = req.body;
+
+  // const usuario = {};
+
+  usuarios.push({
+    id: Number(id),
+    username,
+    name,
+  });
+
+  res.json({
+    status: "Creado",
+  });
+});
+
+app.listen(5000, () => console.log("Server listening on port 5000"));
